3bc5c698e84d8bcc4199a919203baf53b72600c6
pandora267 <greta.kazlauskaite@setupad.com>
2026-07-17 16:12:15 +0300

    Align LEA parser with official long format: tests/Unit/LeaWorkbookParserTest.php

--- tests/Unit/LeaWorkbookParserTest.php
+++ tests/Unit/LeaWorkbookParserTest.php
@@ -9,23 +9,23 @@ use PHPUnit\Framework\TestCase;
 
 final class LeaWorkbookParserTest extends TestCase
 {
-    public function testItMapsFuelColumnsWithoutMixingPb95AndPb98(): void
+    public function testItAggregatesTheOfficialLongFormatWithoutMixingFuelTypes(): void
     {
         $parsed = (new LeaWorkbookParser())->parse(
             dirname(__DIR__) . '/Fixtures/lea_prices_valid.xlsx',
         );
 
-        self::assertSame(4, $parsed->rawRowCount);
-        self::assertSame(['pb95', 'pb98', 'diesel', 'lpg'], $parsed->detectedFuelSlugs);
+        self::assertSame(12, $parsed->rawRowCount);
+        self::assertSame(['pb95', 'diesel', 'lpg'], $parsed->detectedFuelSlugs);
         self::assertCount(4, $parsed->stations);
         self::assertSame([], $parsed->issues);
+        self::assertSame(['2026-07-17'], $parsed->sourceDates);
 
         $first = $parsed->stations[0];
         self::assertSame('Circle K', $first['brand']);
         self::assertSame('Vilnius', $first['city']);
         self::assertSame('Vilniaus m. sav.', $first['municipality']);
         self::assertSame(1.499, $first['prices']['pb95']);
-        self::assertSame(1.599, $first['prices']['pb98']);
         self::assertSame(1.399, $first['prices']['diesel']);
         self::assertSame(0.699, $first['prices']['lpg']);
     }
